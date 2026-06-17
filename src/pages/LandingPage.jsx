f6d3838e71949c975ed52a14ca5c86d5d290c44f
rickz20 <riquellme40@gmail.com>
2026-06-16 21:41:43 -0300

    Landing page
    completando landing page, responsividade

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,6 +6,8 @@ import gramaImg from "../assets/Group 112.png";
 import laranja from "../assets/laranja 1 1.png";
 import separador1 from "../assets/separador1.png";
 import separador2 from "../assets/separador2.png";
+import maca1 from "../assets/maça 1.png";
+import fundoCTA from "../assets/fundocta.png";
 
 export default function LandingPage() {
     return (
@@ -53,54 +55,54 @@ export default function LandingPage() {
                     <div className="beneficios-itens">
                         <div>
                             <div className="beneficios-item">
-                            <div className="numero"> <h1>01</h1> </div>
-                            <div>
-                                <h3>Receitas fáceis de verdade</h3>
-                                <p>Pratos simples, gostosos e práticos pra fazer no dia a dia, com ingredientes acessíveis e sem complicação.</p>
+                                <div className="numero"> <h1>01</h1> </div>
+                                <div>
+                                    <h3>Receitas fáceis de verdade</h3>
+                                    <p>Pratos simples, gostosos e práticos pra fazer no dia a dia, com ingredientes acessíveis e sem complicação.</p>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="beneficios-item">
-                           <div className="numero"> <h1>02</h1> </div>
-                            <div>
-                                <h3>Dicas sem neura</h3>
-                                <p>Aprenda mais sobre alimentação com conteúdos leves, úteis e fáceis de entender. Sem terrorismo nutricional.</p>
+                            <div className="beneficios-item">
+                                <div className="numero"> <h1>02</h1> </div>
+                                <div>
+                                    <h3>Dicas sem neura</h3>
+                                    <p>Aprenda mais sobre alimentação com conteúdos leves, úteis e fáceis de entender. Sem terrorismo nutricional.</p>
+                                </div>
                             </div>
-                        </div>
 
 
                         </div>
 
-                       <div>
+                        <div>
 
-                         <div className="beneficios-item">
-                            <div className="numero"> <h1>03</h1> </div>
-                            <div>
-                                <h3>Nutricionistas por perto</h3>
-                                <p>Encontre profissionais e atendimento especializado de forma rápida, simples e sem dor de cabeça.</p>
+                            <div className="beneficios-item">
+                                <div className="numero"> <h1>03</h1> </div>
+                                <div>
+                                    <h3>Nutricionistas por perto</h3>
+                                    <p>Encontre profissionais e atendimento especializado de forma rápida, simples e sem dor de cabeça.</p>
+                                </div>
                             </div>
-                        </div>
 
 
-                        <div className="beneficios-item">
-                            <div className="numero"> <h1>04</h1> </div>
-                            <div>
-                                <h3>Feito pra vida real</h3>
-                                <p>A Nutra se adapta à sua rotina, ao seu tempo e ao seu bolso. Sem pressão, sem culpa e do seu jeito.</p>
+                            <div className="beneficios-item">
+                                <div className="numero"> <h1>04</h1> </div>
+                                <div>
+                                    <h3>Feito pra vida real</h3>
+                                    <p>A Nutra se adapta à sua rotina, ao seu tempo e ao seu bolso. Sem pressão, sem culpa e do seu jeito.</p>
+                                </div>
                             </div>
-                        </div>
 
-                       </div>
+                        </div>
                     </div>
                 </div>
-                                    <img src={separador2} alt="separador 2" className="separador separador2" />
+                <img src={separador2} alt="separador 2" className="separador separador2" />
 
             </section>
 
             {/* COMO FUNCIONA */}
             <section>
-                <div>
-                    <div>
+                <div className="comoFunciona-container">
+                    <div className="comoFunciona-conteudo">
                         <div>
                             <h2>Como o Nutra funciona?</h2>
                             <p>
@@ -112,23 +114,25 @@ export default function LandingPage() {
                             </p>
                         </div>
                         <div>
-                            <div>Character Placeholder</div>
+                            <img src={maca1} alt="maça" />
                         </div>
                     </div>
                 </div>
             </section>
 
             {/* CTA */}
-            <section>
-                <div>
+            <section className="cta-section">
+                <div className="cta-container">
                     <p>Comece a cuidar da alimentação de um jeito mais leve</p>
                     <h2>
                         Venha para o Nutra e descubra como comer melhor pode ser mais simples.
                     </h2>
-                    <button>Faça seu cadastro</button>
+                    <button className="cta-button">Faça seu cadastro</button>
                 </div>
-            </section>
 
+                {/* Imagem com a nova classe */}
+                <img src={fundoCTA} alt="fundo cta" className="cta-fundo-img" />
+            </section>
         </div>
     );
 }
